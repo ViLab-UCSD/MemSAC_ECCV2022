898391c2b33d6d214bb652a0480295605c9201e1
Tarun <tannu.2354@gmail.com>
2022-07-15 18:29:54 -0700

    Code Release

---
+++ load_images.py
@@ -0,0 +1,99 @@
+#from __future__ import print_function, division
+
+import numpy as np
+from PIL import Image
+import os.path as osp
+
+def make_dataset(image_list, labels, use_path_for_labels = False):
+    if(use_path_for_labels):
+        all_make_ids = [img.split("/")[0] for img in image_list]
+        set_make_ids = list(set(all_make_ids))
+        make_to_class = {idx: _id for idx, _id in enumerate(set_make_ids)}
+        images = [( img, make_to_class[img.split("/")[0]] ) for img in image_list]
+        return images
+
+    if labels:
+        len_ = len(image_list)
+        images = [(image_list[i].strip(), labels[i, :]) for i in range(len_)]
+    else:
+        if len(image_list[0].split()) > 2:
+            images = [(val.split()[0], np.array([int(la) for la in val.split()[1:]])) for val in image_list]
+        else:
+            images = [(val.split()[0], int(val.split()[1])) for val in image_list]
+    return images
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path):
+    #from torchvision import get_image_backend
+    #if get_image_backend() == 'accimage':
+    #    return accimage_loader(path)
+    #else:
+        return pil_loader(path)
+
+
+class ImageList(object):
+    """
+    Args:
+        root (string): Root directory path.
+        image_list (file_handler) : File containing list of files in the format <impath> <label>
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+     Attributes:
+        classes (list): List of the class names.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __init__(self, root, image_list, labels=None, transform=None, target_transform=None,
+                 loader=default_loader, dataset = None):
+        use_path_for_labels = True if dataset == 'compcars' else False
+        imgs = make_dataset(image_list, labels, use_path_for_labels=use_path_for_labels)
+
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+        self.root = root
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        path, target = self.imgs[index]
+        path = osp.join(self.root, path)
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
---
+++ memory.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import contrastive as cl
+
+class MemoryModule(nn.Module):
+    def __init__(self, dim, K=65536, m=0.999, T=0.007, knn=5, top_ranked_n=32, ranking_k=4, knn_method="ranking", batch_size=32, similarity_func="cosine"):
+        """
+        dim: feature dimension (default: 128)
+        K: queue size (default: 65536)
+        T: softmax temperature (default: 0.07)
+        """
+        super().__init__()
+
+        self.K = K
+        self.m = m
+        self.T = T
+
+        sim_config = {
+        'similarity_func': similarity_func,
+        'top_n_sim'      : knn,
+        'ranking_k'      : ranking_k,
+        'top_ranked_n'   : top_ranked_n,
+        'knn_method'     : knn_method,
+        'batch_size'     : batch_size,
+        'tau'            : T
+            }
+
+        sim_module = cl.MSCLoss(sim_config)
+        self.sim_module = sim_module.cuda()
+
+        self.queue_labels = nn.Parameter(data=torch.zeros(K, dtype=torch.long), requires_grad=False)
+        self.queue = nn.Parameter(data=torch.zeros(K , dim), requires_grad=False)
+        self.queue_ptr = nn.Parameter(torch.zeros(1,dtype=torch.long), requires_grad=False)
+
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys, key_labels): 
+
+        batch_size = keys.shape[0]
+        
+        ptr = int(self.queue_ptr)
+
+        assert self.K % batch_size == 0  ## Just makes life easier. https://github.com/facebookresearch/moco/blob/78b69cafae80bc74cd1a89ac3fb365dc20d157d3/moco/builder.py#L60 
+
+        self.queue[ptr:ptr + batch_size, :] = keys
+        self.queue_labels[ptr:ptr + batch_size] = key_labels
+        ptr = (ptr + batch_size) % self.K  # move pointer
+
+        self.queue_ptr[0] = ptr
+
+    def forward(self, features, source_labels):
+
+        source_features = features[:source_labels.shape[0]]
+        target_features = features[source_labels.shape[0]:]
+        self.key_features = source_features.detach()
+
+        # dequeue and enqueue
+        sim_loss = self.sim_module(self.queue, self.queue_labels, target_features)
+        self._dequeue_and_enqueue(self.key_features, source_labels)
+        return sim_loss
